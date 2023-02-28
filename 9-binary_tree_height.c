bd7d2c59d3e5d0c3eceed4f97028bb73bdae4026
David Egwuatu <davidegwuatu2@gmail.com>
2023-02-28 15:14:34 +0100

    finished task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left = 0, right = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
+	else
+		left += 0;
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
+	else
+		right += 0;
+	if (left > right)
+		return (left);
+	return (right);
+}
